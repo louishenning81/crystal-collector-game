fd242fe125e8eedac7ba710fec97f72eae31ed35
louishenning81 <lhhenning@gmail.com>
2019-02-27 08:42:14 -0800

    created variables and function to randomize the gold bar count, need to call function and create a way to add up selections

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,31 @@
 $(document).ready(function() {
 
   // Initial Variables
-  var computerPick = Math.floor(Math.random() * 100) + 1;
+  var computerGold = Math.floor(Math.random() * 100) + 10;
   var lockGame = false;
+  var roseGold = "";
+  var whiteGold = "";
+  var yellowGold = "";
+  var greenGold = ""; 
+  var myGold = "";
+
 
   // We log the computer's pick to console to make it easier to troubleshoot
-  console.log("Computer Pick: " + computerPick);
+  console.log("Computer Pick: " + computerGold);
+  $("#computer-pick").text(computerGold);
 
+//   Function
+function goldBarCount () {
+    roseGold = (Math.floor(Math.random() * 9) + 1);
+    whiteGold = (Math.floor(Math.random() * 9) + 1);
+    yellowGold = (Math.floor(Math.random() * 9) + 1);
+    greenGold = (Math.floor(Math.random() * 9) + 1);      
+}
+  
   // Here we create the on click event that gets the users pick
   $(".btn-choice").on("click", function() {
 
+    goldBarCount();
     // Here this lockGame line prevents the user from changing the option after the game is done.
     if (lockGame !== true) {
 
@@ -18,11 +34,10 @@ $(document).ready(function() {
       var yourPick = $(this).val();
       console.log("Your Pick: " + yourPick);
 
-      // We then reveal the computer's pick in the html
-      $("#computer-pick").text(computerPick);
+
 
       // If your pick matched the computer's pick you let them know.
-      if (parseInt(yourPick) === computerPick) {
+      if (parseInt(yourPick) === computerGold) {
         $("#result").text("Yep! You got it! Refresh the page to play again.");
         lockGame = true;
       }
