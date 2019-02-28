347149da6f8b9e0f3564478b4f577fb004421c0f
louishenning81 <lhhenning@gmail.com>
2019-02-27 17:15:29 -0800

    logic is all done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,9 @@ $(document).ready(function () {
     var yellowGold = "";
     var greenGold = "";
     var myGold = 0;
-
+    var iLoveGold = new Audio("assets/Gold.mp3")
+    var wins = 0;
+    var losses = 0;
 
     // We log the computer's pick to console to make it easier to troubleshoot
     console.log("Computer Pick: " + computerGold);
@@ -17,63 +19,75 @@ $(document).ready(function () {
 
     //   Function
     function goldBarCount() {
-        roseGold = (Math.floor(Math.random() * 9) + 1);
+        roseGold = (Math.floor(Math.random() * 20) + 1);
 
         console.log(roseGold);
-        whiteGold = (Math.floor(Math.random() * 9) + 1);
+        whiteGold = (Math.floor(Math.random() * 20) + 1);
         console.log(whiteGold);
-        yellowGold = (Math.floor(Math.random() * 9) + 1);
+        yellowGold = (Math.floor(Math.random() * 20) + 1);
         console.log(yellowGold);
-        greenGold = (Math.floor(Math.random() * 9) + 1);
+        greenGold = (Math.floor(Math.random() * 20) + 1);
         console.log(greenGold);
+        
     }
 
     goldBarCount();
-
+    //  iLoveGold.play();
     // Here we create the on click event that gets the users pick
     $(".btn-choice").on("click", function () {
 
-        // Here this lockGame line prevents the user from changing the option after the game is done.
-        if (lockGame !== true) {
 
-            // We get the value associated with the button the user picked from here
-            var yourPick = $(this).val();
 
+        // We get the value associated with the button the user picked from here
+        var yourPick = $(this).val();
 
 
 
 
 
-            // If your pick matched the computer's pick you let them know.
-            if (myGold < computerGold) {
-                if (yourPick === "1") {
-                    myGold += yellowGold;
-                    $("#myGold").text(myGold);
-                    console.log(myGold)
-                } else if (yourPick === "2") {
-                    myGold += whiteGold;
-                    $("#myGold").text(myGold);
-                } else if (yourPick === "3") {
-                    myGold += roseGold;
-                    $("#myGold").text(myGold);
-                } else if (yourPick === "4") {
-                    myGold += greenGold;
-                    $("#myGold").text(myGold);
-                }
-                if (myGold === computerGold) {
-                    $("#myGold").text("Yep! You got it! Refresh the page to play again.");
-                    lockGame = true;
-                } else if (myGold > computerGold) {
-                    $("#myGold").text("Nope. Refresh the page to play again.");
-                    lockGame = true;
-                }
+
+        // If your pick matched the computer's pick you let them know.
+        if (myGold < computerGold) {
+            if (yourPick === "1") {
+                myGold += yellowGold;
+                $("#myGold").text(myGold);
+            } else if (yourPick === "2") {
+                myGold += whiteGold;
+                $("#myGold").text(myGold);
+            } else if (yourPick === "3") {
+                myGold += roseGold;
+                $("#myGold").text(myGold);
+            } else if (yourPick === "4") {
+                myGold += greenGold;
+                $("#myGold").text(myGold);
             }
+            if (myGold === computerGold) {
+                $("#myGold").text("Yep! You got it! Refresh the page to play again.");
+                wins++;
+                $("#Wins").html("Wins: " + wins);
+                myGold = 0;
+                computerGold = Math.floor(Math.random() * 100) + 20;
+                $("#computer-pick").text(computerGold);
+                goldBarCount();
+                
+
+            } else if (myGold > computerGold) {
+                $("#myGold").text("Nope. Refresh the page to play again.");
+                losses++;
+                $("#Losses").html("Losses: " + losses);
+                myGold = 0;
+                computerGold = Math.floor(Math.random() * 100) + 20;
+                $("#computer-pick").text(computerGold);
+                goldBarCount();
+
+            }
+        }
 
 
-            // If the numbers did not match. You also let them know
+        // If the numbers did not match. You also let them know
 
 
 
-        };
-    });
-});
+    }
+    )
+})
